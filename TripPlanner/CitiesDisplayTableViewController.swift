73c307f29acc06325d1f96393c655f9c1ffb0977
Rachit1313 <Rachitchawla33@gmail.com>
2024-04-12 18:09:06 -0400

    Fixed typo

--- TripPlanner/CitiesDisplayTableViewController.swift
+++ TripPlanner/CitiesDisplayTableViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class CitiesDsiplayTableViewController: UITableViewController {
+class CitiesDisplayTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
